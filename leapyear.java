212572bc40223e74d0b65ece6fa2fd17e97355e1
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-25 16:29:48 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,16 @@
+
+public class leapyear {
+    public static void main(String[] args) {
+        int year = 2024;
+        boolean isLeap = checkLeapYear(year);
+        if (isLeap) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean checkLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
